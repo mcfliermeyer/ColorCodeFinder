cbd894a912931e130c9c7dbc4416905b446ebca0
Mark Meyer <mcfliermeyer@gmail.com>
2022-10-11 21:57:52 -0400

    CopperPairSvgComponent with react-native-svg implement.

---
+++ CopperPairSvgComponent.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import SVG, { Path } from "react-native-svg";
+
+const CopperPairSvgComponent = () => (
+  <SVG
+    viewBox="18 23.621 40 80"
+    width={40}
+    height={180}
+    // {...props}
+  >
+    <Path
+      strokeLinecap="round"
+      stroke="#090dfa"
+      strokeWidth="13"
+      d="M61.216 152.744c-6.265-26.486-19.598-35.294-23.101-68.917C34.892 52.898 57.2 23.113 58.395 1.968"
+      transform="rotate(180 43.134 88.183)"
+    />
+    <Path
+      strokeLinecap="round"
+      stroke="red"
+      strokeWidth="12"
+      d="M53.069 23.751c-3.2 26.368-20.841 35.457-24.344 69.08-3.223 30.929 19.195 60.033 20.39 81.178"
+    />
+    <Path
+      stroke="#090dfa"
+      strokeLinecap="round"
+      strokeWidth="12.8px"
+      d="m43.244 125.565-10 28"
+      transform="rotate(.999 38.242 139.566)"
+    />
+  </SVG>
+);
+
+export default CopperPairSvgComponent;
