54f2161daf7d1741317d2484a92aba23fb4e3163
Mark Meyer <mcfliermeyer@gmail.com>
2022-10-20 07:08:36 -0400

    Full page of pairs with spacing and correct color code.

--- components/CopperPairSelector.tsx
+++ components/CopperPairSelector.tsx
@@ -8,36 +8,59 @@ const screen = Dimensions.get("screen");
 // TODO figure out why 100% height is not filling whole screen
 
 const CopperPairSelector = () => {
+  const padding = 33;
+  const componentWidth = screen.width / 5 - padding * 2;
+  const componentHeight = screen.height / 5 - padding * 2;
+  console.log(componentWidth);
   return (
-    <View style={styles.container}>
-        {Array.from({ length: 5 }, (_, index) => (
-          <TouchableOpacity style={styles.pairButton} key={index}>
-            <CopperPairSvgComponent
-              tipColor={useTipColor(1)}
-              ringColor={useRingColor(1)}
-              width={screen.width / 20}
-              height={screen.height / 5}
-            />
-          </TouchableOpacity>
-        ))}
+    <View style={styles.pairsContainer}>
+      {Array.from({ length: 5 }, (_, outter_index) => (
+        <View style={styles.wrapper} key={outter_index}>
+          {Array.from({ length: 5 }, (_, inner_index) => (
+            <TouchableOpacity
+              style={styles.pairButton}
+              key={inner_index * outter_index}
+            >
+              <CopperPairSvgComponent
+                tipColor={useTipColor((inner_index + 1) + (5 * outter_index))}
+                ringColor={useRingColor((inner_index + 1))}
+                width={componentWidth}
+                height={componentHeight}
+              />
+            </TouchableOpacity>
+          ))}
+        </View>
+      ))}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  pairsContainer: {
+    // flex: 1,
+    // flexDirection: "column",
+    // height: "100%",
+    // width: "100%",
+    position: "absolute",
+    padding: 20,
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  wrapper: {
     flex: 1,
     flexDirection: "row",
+    paddingHorizontal: 20,
     // backgroundColor: "#36393e",
-    minHeight: "100%",
-    minWidth: "100%",
+    maxWidth: "100%",
     backgroundColor: "peru",
     alignItems: "center",
     justifyContent: "center",
   },
   pairButton: {
-    padding: 25,
-  }
+    padding: 28,
+  },
 });
 
 export default CopperPairSelector;
