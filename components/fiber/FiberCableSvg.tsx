2775dcccf5dfaeb45f019617327daab6d2059a20
Mark Meyer <mcfliermeyer@gmail.com>
2022-10-30 20:38:19 -0400

    Fiber turns to background color if under 1.

--- components/fiber/FiberCableSvg.tsx
+++ components/fiber/FiberCableSvg.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { TouchableOpacity } from "react-native";
-import SVG, { Path, Rect, Polyline } from "react-native-svg";
+import SVG, { Path, Rect, } from "react-native-svg";
 import useFiberColor from "../../hooks/useFiberColor";
 
 interface Props {
@@ -20,12 +20,12 @@ const FiberCableSvg = (props: Props) => {
     >
       <Path
         strokeWidth={9}
-        stroke={useFiberColor(props.fiber - 2)}
+        stroke={props.fiber - 2 === 0 ? "#424549" : useFiberColor(props.fiber - 2)}
         d="m6.373 37 .423 36.523"
         transform="translate(1.127)"
       />
       <Path
-        stroke={useFiberColor(props.fiber - 1)}
+        stroke={props.fiber - 1 === 0 ? "#424549" : useFiberColor(props.fiber - 1)}
         strokeWidth={9}
         origin="0.5 0.36"
         d="m13.648 22 .423 50"
