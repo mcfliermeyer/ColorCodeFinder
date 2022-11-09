c03a40c54e20b3e4e3047883d0740715ce2773ae
Mark Meyer <mcfliermeyer@gmail.com>
2022-11-09 07:05:24 -0500

    Start Flatlist implementation.

--- components/fiber/FiberCableSvg.tsx
+++ components/fiber/FiberCableSvg.tsx
@@ -1,9 +1,7 @@
-import { useRef } from "react";
-import { Animated, ViewProps } from "react-native";
-import SVG, { Path, Rect, G, SvgProps } from "react-native-svg";
+import SVG, { Path, Rect } from "react-native-svg";
 import useFiberColor from "../../hooks/useFiberColor";
 import hexToHSL from "../utilities/hexToHSL";
-
+import { StyleSheet } from "react-native";
 interface Props {
   height?: number;
   width?: number;
@@ -19,20 +17,8 @@ const highlightStrandSvg = (
   const lightenFiber = l + 25;
 
   return [...Array(howManyStrokes)].map((e, i) => {
-    const redlightAlphaStepDown = 0.15 / howManyStrokes;
-    const redlightAlpha = 0.15 - redlightAlphaStepDown * i;
-    const startYStepUp = -2.7 / howManyStrokes;
-    const currentStartY = -2.7 + startYStepUp * i;
-    const endYStepUp = -5 / howManyStrokes;
-    const currentEndY = -5 - endYStepUp * i;
     return (
-      <G key={i}>
-        {/* redlight*/}
-        {/* <Path
-          stroke={`hsl(0, 100%, 50%, ${redlightAlpha})`}
-          strokeWidth={0.9}
-          d={`M32.5 ${currentStartY} l0 0 0 ${currentEndY}`}
-        /> */}
+      <SVG key={i}>
         {/* main column of fiber */}
         <Path
           stroke={`hsl(${h}, ${s}%, ${lightenFiber + i}%, ${1.0 - i / 12})`}
@@ -50,26 +36,24 @@ const highlightStrandSvg = (
               C${32.5 - i * strokeSize} 0 32.5 -5 ${32.5 + i * strokeSize} 0
             `}
         />
-      </G>
+      </SVG>
     );
   });
 };
 
 const FiberCableSvg = (props: Props) => {
   return (
-    <SVG
-      viewBox="0 0 66 171"
-      width={props.width}
-      height={props.height}
-    >
+    <SVG viewBox="0 0 66 171" width={props.width} height={props.height}>
       {highlightStrandSvg(10, 0.12, useFiberColor(props.fiber))}
-      <Path
-        strokeWidth={9}
-        stroke={
-          props.fiber - 2 === 0 ? "#424549" : useFiberColor(props.fiber - 2)
-        }
-        d="m7.3 50 0 25"
-      />
+      <SVG style={styles.fiberOne}>
+        <Path
+          strokeWidth={9}
+          stroke={
+            props.fiber - 2 === 0 ? "#424549" : useFiberColor(props.fiber - 2)
+          }
+          d="m7.3 50 0 25"
+        />
+      </SVG>
       <Path
         stroke={
           props.fiber - 1 === 0 ? "#424549" : useFiberColor(props.fiber - 1)
@@ -111,3 +95,14 @@ const FiberCableSvg = (props: Props) => {
   );
 };
 export default FiberCableSvg;
+
+const styles = StyleSheet.create({
+  fiberOne: {
+    position: "absolute",
+    bottom: 10,
+    left: 0,
+    right: 10,
+    top: 20,
+    // backgroundColor: "yellow",
+  },
+});
---
+++ components/fiber/ScrollableFiber.tsx
@@ -0,0 +1,6 @@
+type Props = {};
+const ScrollableFiber = (props: Props) => {
+  return <div>ScrollableFiber</div>;
+};
+export default ScrollableFiber;
+//needing to add flatlist scroll with svg painted on each component
